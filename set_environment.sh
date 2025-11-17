034a50d50220bb52a6b6811860bdd3d158f7c7f3
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2025-11-17 17:30:20 +0000

    Ongoing development.

--- set_environment.sh
+++ set_environment.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-module load dev_tools/oct2017/python-Anaconda-3-5.0.0.1 
+module load dev_tools/oct2017/python-Anaconda-3-5.0.0.1
+module load astro/dec2019/chainconsumer-0.30 
--- utility.py
+++ utility.py
@@ -19,28 +19,40 @@ def make_plots():
     
     x_tilda = 3.0
     y_tilda = 4.0
+    
+    lower_limit = -0.5
+    upper_limit = 6.5
 
 
 
-    #plt.plot([1,2,3,4], [1,4,9,16], c='red')
-    plt.xlim(-0.5, 6.5)
-    plt.ylim(-0.5, 6.5)
+    plt.xlim(lower_limit, upper_limit)
+    plt.ylim(lower_limit, upper_limit)
     plt.gca().set_aspect('equal')
     
     
     # Observed data
     if True:
         plt.plot([x_tilda,], [y_tilda,], 'or')
+        plt.savefig('./figure_1.png', bbox_inches="tight")
     
     if True:
         num_true_points = 1500
         x_trues = np.random.normal(loc=x_tilda, scale=1.0, size=num_true_points)
         y_trues = np.random.normal(loc=y_tilda, scale=1.0, size=num_true_points)
         plt.plot(x_trues, y_trues, '.g', ms=1)
+        plt.savefig('./figure_2.png', bbox_inches="tight")
+        
+    if True:
+        plt.plot([lower_limit, upper_limit], [0.75 * y for y in [lower_limit, upper_limit]], 'k-')
+        plt.text(6.2, 4.3, r'$b$')
+        plt.plot([lower_limit, upper_limit], [0.80 * y for y in [lower_limit, upper_limit]], 'k-')
+        plt.text(5.3, 5, r'$b+db$')
+        plt.savefig('./figure_3.png', bbox_inches="tight")
+        
         
     
     
-    plt.savefig('./figure_1.png', bbox_inches="tight")
+    
     plt.show()
 
 
